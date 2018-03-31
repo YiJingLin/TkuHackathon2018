5b0db861a08a4f64603d1529d1af166946f08ac6
Peter <peter09830961@gmail.com>
2018-03-31 20:23:19 +0800

    mission add google map

--- public/assets/js/mission.js
+++ public/assets/js/mission.js
@@ -54,3 +54,45 @@ function imageFileOnChange(input){
     }
     reader.readAsDataURL(input.files[0]);
 }
+
+function initMap() {
+    if($('input[name="address"]').val() != ""){
+        marking();
+    } else {
+        var uluru = {lat: 23.4857501, lng: 120.0843006};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: uluru
+        });
+    }
+}
+
+function marking(){
+    var htmlAddress = "https://maps.googleapis.com/maps/api/geocode/json?address="+$('input[name="address"]').val()+"&key=AIzaSyDU1RvYVezuY1WPX5lqX__InSPPGRTRH9w";
+    $.ajax({
+        url: htmlAddress,
+        processData: false,
+        contentType: false,
+        type: "POST",
+        success: function(msg) {
+            console.log(msg);
+            if(msg['status'] == "OK"){
+                var pos = msg['results'][0]["geometry"]["location"];
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 12,
+                    center: pos
+                });
+                var marker = new google.maps.Marker({
+                    position: pos,
+                    map: map
+                });
+                console.log("success");
+            } else {
+                console.log("error");
+            }
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log("error");
+        }
+    });
+}
